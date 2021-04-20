f9fbd758d8c3eeeedb29e1154b3baf4a16f18b10
caundurragas <caundurragas@gmail.com>
2021-04-20 19:24:17 +0100

    added the API of the Daily Forecast

--- src/app.js
+++ src/app.js
@@ -40,6 +40,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconWeather.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function serchToGetApi(city) {
@@ -83,6 +85,13 @@ fahrenheit.addEventListener("click", displayFahrenheit);
 let celcius = document.querySelector("#celsius");
 celcius.addEventListener("click", displayCelsius);
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "3ec7bf82a84873e82215df15af12d134";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayDailyForecast);
+}
+
 function displayDailyForecast() {
   let dailyForecast = document.querySelector("#daily-forecast");
   let dailyForecastHTML = `<div class= "row">`;
@@ -111,5 +120,4 @@ function displayDailyForecast() {
   dailyForecast.innerHTML = dailyForecastHTML;
 }
 
-displayDailyForecast();
 serchToGetApi("London");
